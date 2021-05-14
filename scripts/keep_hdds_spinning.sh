87279c33b26c11113efbeb7edcdce8bce8491989
Jernej Makovsek <jernej.makovsek@gmail.com>
2021-05-15 00:25:17 +0200

    Fix script

--- scripts/keep_hdds_spinning.sh
+++ scripts/keep_hdds_spinning.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find $1 -mindepth 1 -maxdepth 1 -type d | xargs -I {} ^C -c 'date > {}/.chia-manager-keep_hdd_spinning'
+find $1 -mindepth 1 -maxdepth 1 -type d | xargs -I {} sh -c 'date > {}/.chia-manager-keep_hdd_spinning'
